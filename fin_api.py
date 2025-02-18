91c089056b91c0d88d8055aa7d6d08423948172b
malex30 <mattagranado@gmail.com>
2025-02-17 23:39:51 -0600

    "Add postgres db configs, and looking at stock news"

--- fin_api.py
+++ fin_api.py
@@ -1,14 +1,36 @@
 import finnhub
 import os
+import pendulum
+import psycopg2
 from icecream import ic
 from dotenv import load_dotenv
 
 load_dotenv()
 
+DB_HOST = os.getenv("DB_HOST")
+DB_PORT = os.getenv("DB_PORT")
+DB_NAME = os.getenv("DB_NAME")
+DB_USER = os.getenv("DB_USER")
+DB_PASSWORD = os.getenv("DB_PASSWORD")
+
+# Establish the connection
+conn = psycopg2.connect(
+    host=DB_HOST, port=DB_PORT, dbname=DB_NAME, user=DB_USER, password=DB_PASSWORD
+)
+
+
 finnhub_api_key = os.getenv("FINNHUB_API_KEY")
 
 # Create a proper client instance
 finnhub_client = finnhub.Client(api_key=finnhub_api_key)
+
+now = pendulum.now()
+sunday = now.previous(pendulum.SUNDAY)
+previous_sunday = sunday.subtract(days=7)
+now = now.format("YYYY-MM-DD")
+sunday = sunday.format("YYYY-MM-DD")
+previous_sunday = previous_sunday.format("YYYY-MM-DD")
+ic(now, sunday, previous_sunday)
 
 print(type(finnhub_client))
 
@@ -44,3 +66,8 @@ stocks = {
 for company, ticker in stocks.items():
     profile = finnhub_client.company_profile2(symbol=ticker)
     ic(company, profile)
+
+
+for company, ticker in stocks.items():
+    news = finnhub_client.company_news(symbol=ticker, _from=previous_sunday, to=sunday)
+    ic(company, ticker, news)
